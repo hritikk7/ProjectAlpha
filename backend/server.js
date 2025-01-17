bac0d8d5215f7d4fd11e83171ef69591bc16a1d7
Ritik <54003516+hritikk7@users.noreply.github.com>
2025-01-18 00:06:24 +0530

    Feature/backend setup (#5)
    * backend setup done
    
    * adding gitignore file
    
    * modified git ignore file
    
    * removed catched node_modules and modified gitignore

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+const PORT = 5000;
+
+app.get('/', (req, res)=>[
+  res.send("helloo")
+
+])
+
+
+app.listen(PORT, ()=>{
+  console.log(`Listning on Port: ${PORT}`)
+  
+})
