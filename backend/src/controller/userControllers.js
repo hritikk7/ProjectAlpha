d0e329a1341e44fee9ba2d779d2e30debddd6aef
hritikk7 <kumarhritik158@gmail.com>
2025-01-23 00:29:44 +0530

    removed backend

--- backend/src/controller/userControllers.js
+++
@@ -1,7 +0,0 @@
-export const registerUser = async (req, res, next) => {
-  const {email, pasword, name} = req.body
-  res.json({
-    message: "Welcom user",
-  });
-};
-
--- backend/src/routes/userRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express")
-const registerUser = require("../../controller/userControllers")
-
-const userRouter = express.Router
-
-userRouter.post('/register', registerUser)
